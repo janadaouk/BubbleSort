2afb43cccd982731f4c5e01cfa363d31b72f3fc3
jana-daouk <jana.daouk@student.allenisd.org>
2022-10-08 01:07:24 -0500

    2nd commit

--- main.swift
+++ main.swift
@@ -1,4 +1,5 @@
-
+import Foundation
+/*
 
 // ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
 // DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
@@ -8,14 +9,18 @@ let unsortedIntegers = [5, 1, 4, 2, 8]
 
 
 // Add your code below:
-var array = unsortedIntegers
-func swap(integers: inout [Int], firstIndex: Int, secondIndex: Int) {
+//var array = unsortedIntegers
+
+var unsorted = [String]()
+var input: String
+*/
+func swap(integers: inout [String], firstIndex: Int, secondIndex: Int) {
    
     let temp = integers[firstIndex]
      integers[firstIndex] = integers[secondIndex]
      integers[secondIndex] = temp
 }
-
+/*
 func sort(array: [Int])    {
 
     var totalswapcount = 0
@@ -23,7 +28,7 @@ func sort(array: [Int])    {
     var array = unsortedIntegers
     var swapcountperpass = 0
 
-    print("Pass: \(passcount), Swaps: \(swapcountperpass)/\(totalswapcount), Array: \(unsortedIntegers)")
+//    print("Pass: \(passcount), Swaps: \(swapcountperpass)/\(totalswapcount), Array: \(unsortedIntegers)")
     repeat {
         swapcountperpass = 0
         passcount += 1
@@ -41,5 +46,54 @@ func sort(array: [Int])    {
         print("Pass: \(passcount), Swaps: \(swapcountperpass)/\(totalswapcount), Array: \(array)")
     } while swapcountperpass != 0
 
-   }
-sort(array: array)
+}
+
+while let input = readLine() {
+    unsorted.append(input)
+}
+sort(array: unsorted)
+ */
+
+var unsorted = [String]()
+var input: String
+
+func compareString(stringOne: String, stringTwo: String) -> Bool {
+
+    let firstString = stringOne.lowercased().filter("abcdefghijklmnopqrstuvwxyz".contains)
+    let secondString = stringTwo.lowercased().filter("abcdefghijklmnopqrstuvwxyz".contains)
+    if firstString > secondString {
+        return true
+    }
+    return false
+}
+
+func sort(array:[String]) {
+    var unsorted = array
+    var swapcountperpass = 0
+    repeat {
+        swapcountperpass = 0    
+        for index in 0 ..< unsorted.count - 1 {
+            let element = unsorted[index]
+            let nextelement = unsorted[index + 1]
+
+if compareString(stringOne: element, stringTwo: nextelement) == true {
+    swap(integers: &unsorted, firstIndex: index, secondIndex: index+1)
+    swapcountperpass += 1
+}
+        }
+    } while swapcountperpass != 0
+let sortedText = unsorted.joined(separator: "\n")
+
+//print(sortedText)
+
+}
+// keeps appending the inserted strings to the unsortedstring until you enter an empty one
+//repeat {
+//  input = readLine()!
+//unsorted.append(input)
+//} while input != ""
+while let input = readLine() {
+    unsorted.append(input)
+}
+
+sort(array: unsorted)
